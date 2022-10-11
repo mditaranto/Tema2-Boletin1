513b55eaacf9635ce133310f95482f85ee3f8d9e
mditaranto <114072009+mditaranto@users.noreply.github.com>
2022-10-11 10:39:15 +0200

    Update Ejercicio8.java

--- Tema2Boletin1/src/boletin1/Ejercicio8.java
+++ Tema2Boletin1/src/boletin1/Ejercicio8.java
@@ -13,28 +13,21 @@ public class Ejercicio8 {
 		System.out.print("Introduzca su nota obtenida: "); //Se le pide al usuario que introduzca un número
 		nota = sc.nextInt(); //Se lee el valor en su variable correspondiente
 		
-		//Mediante un switch valoramos la nota del usuario
-		switch (nota) {
-		case 0, 1, 2, 3, 4 -> { //Si se comprende entre 0 y 4 es suspenso
+		//Mediante un condicional le asignamos un valor a la nota del usuario
+		if (nota<5) {
 			System.out.println("Suspensisimo");
-		}
-		case 5 -> { //Si es 5 es suficiente
+		} else if (nota == 5) {
 			System.out.println("Suficiente");
-		} 
-		case 6 -> { //Si es 6 es bien
+		} else if (nota == 6) {
 			System.out.println("bien");
-		}
-		case 7, 8 -> { //Si es 7 u 8 es notable
+		} else if (nota == 7 || nota == 8) {
 			System.out.println("Notable");
-		}
-		case 9, 10 -> { //Si es 9 o 10 es sobresaliente
+		} else if (nota == 9 || nota == 10) {
 			System.out.println("Sobresaliente");
-		}
-		default -> { /*En caso de introducir un valor que no este entre 0 y 10
+		} else { /*En caso de introducir un valor que no este entre 0 y 10
 					consideramos que la nota es incorrecta*/
 			System.out.println("Nota incorrecta");
 		}
-	}
 		
 		sc.close(); //Se cierra el objeto Scanner
 	}
